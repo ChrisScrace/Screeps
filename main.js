bedd8f0d52e101f1122d77bf20c5720fe5f14bd1
ChrisScrace <chrisscrace@gmail.com>
2018-03-03 16:29:09 +0000

    First Commit

---
+++ main.js
@@ -0,0 +1,32 @@
+var roleHarvester = require('role.harvester');
+var roleBuilder = require('role.builder');
+var roleUpgrader = require('role.upgrader');
+var roleSpawn = require('role.spawn');
+
+module.exports.loop = function () {
+
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    for(var name in Game.spawns){
+        var spawn = Game.spawns[name];
+        roleSpawn.spawn(spawn);
+    }
+    
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
