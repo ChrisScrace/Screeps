ff52ec4c87908461c873582b575f2039de740df4
Chris Scrace <chrisscrace@gmail.com>
2025-10-23 22:05:19 +0100

    update room cache

--- roomCache.js
+++ roomCache.js
@@ -1,40 +1,50 @@
+// Optimized per-tick room cache
 module.exports = {
     init(room) {
         if (!room._cache || room._cache.tick !== Game.time) {
-            room._cache = { tick: Game.time };
-
-            // 1️⃣ Get all structures once
             const allStructures = room.find(FIND_STRUCTURES);
-            room._cache.structures = allStructures;
 
-            // 2️⃣ Categorize by type
-            room._cache.containers = allStructures.filter(s =>
-                s.structureType === STRUCTURE_CONTAINER || s.structureType === STRUCTURE_STORAGE
-            ).filter(s => s.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+            room._cache = {
+                tick: Game.time,
 
-            room._cache.towers = allStructures.filter(s => s.structureType === STRUCTURE_TOWER);
-            room._cache.spawns = allStructures.filter(s => s.structureType === STRUCTURE_SPAWN);
-            room._cache.extensions = allStructures.filter(s => s.structureType === STRUCTURE_EXTENSION)
-                .filter(s => s.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+                // Dynamic objects
+                dropped: room.find(FIND_DROPPED_RESOURCES),
+                tombstones: room.find(FIND_TOMBSTONES, { filter: t => t.store[RESOURCE_ENERGY] > 0 }),
+                hostiles: room.find(FIND_HOSTILE_CREEPS),
+                myCreeps: room.find(FIND_MY_CREEPS),
 
-            // 3️⃣ Other room objects
-            room._cache.dropped = room.find(FIND_DROPPED_RESOURCES);
-            room._cache.tombstones = room.find(FIND_TOMBSTONES, { filter: t => t.store[RESOURCE_ENERGY] > 0 });
-            room._cache.sources = room.find(FIND_SOURCES);
-            room._cache.myCreeps = room.find(FIND_MY_CREEPS);
-            room._cache.hostiles = room.find(FIND_HOSTILE_CREEPS);
-            room._cache.constructionSites = room.find(FIND_CONSTRUCTION_SITES);
+                // Structures
+                structures: allStructures,
+                containers: allStructures.filter(s =>
+                    (s.structureType === STRUCTURE_CONTAINER || s.structureType === STRUCTURE_STORAGE) &&
+                    s.store[RESOURCE_ENERGY] > 0
+                ),
+                energyStructures: allStructures.filter(s =>
+                    [STRUCTURE_SPAWN, STRUCTURE_EXTENSION, STRUCTURE_TOWER].includes(s.structureType) &&
+                    s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+                ),
+                repairTargets: allStructures.filter(s =>
+                    (s.hits < s.hitsMax && s.hits < 50000) || s.hits < 2000
+                ),
+
+                // Other
+                sources: room.find(FIND_SOURCES),
+                buildTargets: room.find(FIND_CONSTRUCTION_SITES),
+                upgradeTargets: room.controller && room.controller.my ? [room.controller] : []
+            };
         }
     },
 
-    getContainers(room) { this.init(room); return room._cache.containers; },
-    getTowers(room) { this.init(room); return room._cache.towers; },
-    getSpawns(room) { this.init(room); return room._cache.spawns; },
-    getExtensions(room) { this.init(room); return room._cache.extensions; },
     getDropped(room) { this.init(room); return room._cache.dropped; },
     getTombstones(room) { this.init(room); return room._cache.tombstones; },
+    getContainers(room) { this.init(room); return room._cache.containers; },
+    getEnergyStructures(room) { this.init(room); return room._cache.energyStructures; },
+    getRepairTargets(room) { this.init(room); return room._cache.repairTargets; },
     getSources(room) { this.init(room); return room._cache.sources; },
-    getMyCreeps(room) { this.init(room); return room._cache.myCreeps; },
+    getBuildTargets(room) { this.init(room); return room._cache.buildTargets; },
+    getUpgradeTargets(room) { this.init(room); return room._cache.upgradeTargets; },
     getHostiles(room) { this.init(room); return room._cache.hostiles; },
-    getBuildTargets(room) { this.init(room); return room._cache.constructionSites; }
+    getMyCreeps(room) { this.init(room); return room._cache.myCreeps; },
+    getStructures(room, structureTypes) { this.init(room); return room._cache.structures; },
+
 };
